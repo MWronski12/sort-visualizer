cbea2bb60c78493f2d89b920f5893933991e3453
MWronski12 <wronski.mikolaj@gmail.com>
2022-12-30 18:11:04 +0100

    updated base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import postcss from "./postcss.config.cjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/sort-visualizer/",
   plugins: [svelte()],
   css: {
     postcss,
